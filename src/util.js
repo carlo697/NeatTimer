46b2f83398a96253c8761743d7dc9904e01e561e
Carlos Peña <carlos.07888@gmail.com>
2021-03-09 20:03:31 -0400

    Fix indentation in the function useInterval of the file util.js

--- src/util.js
+++ src/util.js
@@ -35,21 +35,22 @@ export function getTimeSpanStrings(miliseconds) {
 }
 
 export function useInterval(callback, delay) {
-  const savedCallback = useRef();
+	const savedCallback = useRef();
 
-  // Remember the latest callback.
-  useEffect(() => {
-    savedCallback.current = callback;
-  }, [callback]);
+	// Remember the latest callback.
+	useEffect(() => {
+		savedCallback.current = callback;
+	}, [callback]);
 
-  // Set up the interval.
-  useEffect(() => {
-    function tick() {
-      savedCallback.current();
-    }
-    if (delay !== null) {
-      let id = setInterval(tick, delay);
-      return () => clearInterval(id);
-    }
-  }, [delay]);
+	// Set up the interval.
+	useEffect(() => {
+		function tick() {
+			savedCallback.current();
+		}
+
+		if (delay !== null) {
+			let id = setInterval(tick, delay);
+			return () => clearInterval(id);
+		}
+	}, [delay]);
 }
