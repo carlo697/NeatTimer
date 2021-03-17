108c350ed9e63343fba162f7d018d4926a6f5b47
Carlos Peña <carlos.07888@gmail.com>
2021-03-16 20:26:32 -0400

    Add a clock that shows the browser local time

--- src/pages/Clock.js
+++ src/pages/Clock.js
@@ -1,13 +1,34 @@
-import React, {useEffect} from "react";
+import React, {useState, useEffect} from "react";
+import {useInterval} from "../util.js";
 
 const Clock = () => {
+	const [time, setTime] = useState(new Date());
+
 	useEffect(() => {
 		document.title = "Neat Timer - Clock";
 	}, []);
 
+	useInterval(() => {
+		setTime(new Date());
+	}, 100);
+
+	const capitalize = (text) => {
+		return text.charAt(0).toUpperCase() + text.slice(1);
+	};
+
+	const formattedTime = Intl.DateTimeFormat("default", { hour: "numeric", minute: "numeric", second:"numeric", hour12: false }).format(time);
+	const formattedDate = capitalize(Intl.DateTimeFormat("default", { dateStyle: "full"}).format(time));
+
 	return (
 		<main>
-
+			<section>
+				<div className="timer">
+					{formattedTime}
+				</div>
+				<div>
+					{formattedDate}
+				</div>
+			</section>
 		</main>
 	);
 };
