a2395fdaa6fb68919a187c6b8f9ccbdc1ab9a238
Carlos Peña <carlos.07888@gmail.com>
2021-03-18 16:27:52 -0400

    Add Source Code link to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,15 @@
 import React from "react";
+import {IoIosArrowForward} from "react-icons/io";
 
 const Footer = () => {
 	return (
 		<footer className="footer">
+			<div className="footer-links">
+				<a href="https://github.com/carlo697/NeatTimer">
+					<IoIosArrowForward className="icon"/>
+					Source Code
+				</a>
+			</div>
 			<p>NeatTimer {new Date().getFullYear()}</p>
 		</footer>
 	);
